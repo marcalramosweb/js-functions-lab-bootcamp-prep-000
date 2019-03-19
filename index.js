787499db1f3742d31a6843cdb6eae8ae69edf39a
Marcal Ramos <suncastelo@gmail.com>
2019-03-19 14:55:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // write your code below!
+
+function happyHolidays(){
+  return 'Happy holidays!'
+}
+
+happyHolidays()
+
+function happyHolidaysTo(john){
+  return `Happy holidays!, ${john}`
+}
+
+happyHolidaysTo('you!')
+
+function happyHolidayTo(holiday, john){
+  return `Happy ${holiday}, ${john}`
+}
+
+happyHolidayTo('Happy holidays', 'Tonny')
+
+
+function holidayCountdown(travel, numDays){
+  return `It's ${numDays}, until ${travel}`
+}
+
+holidayCountdown(20, 'until Mother\'s Day')
+
